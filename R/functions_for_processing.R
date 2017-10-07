edde01ed7e3c6006055fd8e37256c2e2c772a197
Noah Greifer <noah.greifer@gmail.com>
2017-10-07 17:13:00 -0400

    Added more functions

--- R/functions_for_processing.R
+++ R/functions_for_processing.R
@@ -40,12 +40,23 @@ word.list <- function(word.list = NULL, and.or = c("and", "or"), is.are = FALSE,
 }
 nunique <- function(x) {
   if (is.factor(x)) return(nlevels(x))
-  else return(length(x))
+  else return(length(unique(x)))
+}
+method.to.proper.method <- function(method) {
+  if (method %in% c("ps")) return("ps")
+  else if (method %in% c("gbm", "gbr", "twang")) return("gbm")
+  else if (method %in% c("cbps")) return("cbps")
+  else if (method %in% c("npcbps")) return("npcbps")
+  else if (method %in% c("entropy", "ebal", "ebalance")) return("ebal")
+  else if (method %in% c("sbw")) return("sbw")
+  else if (method %in% c("ebcw", "ate")) return("ebcw")
+  else return(NA_character_)
 }
 method.to.phrase <- function(method) {
   if (method %in% c("ps")) return("propensity score weighting")
   else if (method %in% c("gbm", "gbr", "twang")) return("generalized boosted modeling")
   else if (method %in% c("cbps")) return("covariate balancing propensity score weighting")
+  else if (method %in% c("npcbps")) return("non-parametric covariate balancing propensity score weighting")
   else if (method %in% c("entropy", "ebal", "ebalance")) return("entropy balancing")
   else if (method %in% c("sbw")) return("stable balancing weights")
   else if (method %in% c("ebcw", "ate")) return("empirical balancing calibration weighting")
@@ -62,4 +73,19 @@ process.estimand <- function(estimand, allowable.estimands, method) {
     return(toupper(estimand))
   }
 
+}
+
+between <- function(x, range, inclusive = TRUE, na.action = FALSE) {
+  if (!all(is.numeric(x))) stop("x must be a numeric vector.", call. = FALSE)
+  if (length(range) != 2) stop("range must be of length 2.", call. = FALSE)
+  if (any(is.na(range) | !is.numeric(range))) stop("range must contain numeric entries only.", call. = FALSE)
+  range <- sort(range)
+
+  if (any(is.na(x))) {
+    if (length(na.action) != 1 || !is.atomic(na.action)) stop("na.action must be an atomic vector of length 1.", call. = FALSE)
+  }
+  if (inclusive) out <- ifelse(is.na(x), na.action, x >= range[1] & x <= range[2])
+  else out <- ifelse(is.na(x), na.action, x > range[1] & x < range[2])
+
+  return(out)
 }
