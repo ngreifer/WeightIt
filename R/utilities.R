09bd28a26d79ee296aa82237bb56ff4af812b753
Noah Greifer <noah.greifer@gmail.com>
2020-01-11 02:31:31 -0500

    Deleting unneeded

--- R/utilities.R
+++ R/utilities.R
@@ -44,60 +44,6 @@ make_full_rank <- function(mat, with.intercept = TRUE) {
   else return(as.data.frame(mat[, keep, drop = FALSE]))
 
 }
-.get_w_from_ps <- function(ps, treat, estimand = "ATE", focal = NULL, treated = NULL, subclass = NULL, stabilize = FALSE) {
-  #ps must be a matrix/df with columns named after treat levels
-
-  if (!has.treat.type(treat)) treat <- assign.treat.type(treat)
-
-  treat.type <- get.treat.type(treat)
-
-  processed.estimand <- process.focal.and.estimand(focal, estimand, treat, treat.type, treated)
-  estimand <- processed.estimand$estimand
-  focal <- processed.estimand$focal
-  assumed.treated <- processed.estimand$treated
-
-  if (treat.type == "continuous") {
-    stop("get_w_from_ps can only be used with binary or multinomial treatments.", call. = FALSE)
-  }
-  else {
-    ps <- ps_to_ps_mat(ps, treat, assumed.treated, treat.type, treated, estimand)
-  }
-
-  if (nrow(ps) != length(treat)) {
-    stop("ps and treat must have the same number of units.", call. = FALSE)
-  }
-
-  w <- rep(0, nrow(ps))
-
-  if (is_null(subclass)) {
-    for (i in colnames(ps)) {
-      w[treat == i] <- 1/ps[treat == i, as.character(i)]
-    }
-
-    if (toupper(estimand) == "ATE") {
-      # w <- w
-    }
-    else if (toupper(estimand) == "ATT") {
-      w <- w*ps[, as.character(focal)]
-    }
-    else if (toupper(estimand) == "ATO") {
-      w <- w/apply(ps, 1, function(x) sum(1/x)) #Li & Li (2018)
-    }
-    else if (toupper(estimand) == "ATM") {
-      w <- w*apply(ps, 1, min)
-    }
-    else w <- NULL
-  }
-  else {
-    w <- stratify_ps_and_get_weights(ps, treat, estimand, focal, subclass)
-  }
-
-  if (stabilize) w <- stabilize_w(w, treat)
-
-  names(w) <- rownames(ps)
-
-  return(w)
-}
 get_w_from_ps <- function(ps, treat, estimand = "ATE", focal = NULL, treated = NULL, subclass = NULL, stabilize = FALSE) {
   #ps must be a matrix/df with columns named after treat levels
 
