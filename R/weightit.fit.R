6c8f6554eabdfd88dfd3f076c5523ca3eac0d253
Noah Greifer <noah.greifer@gmail.com>
2020-11-19 01:57:28 -0500

    Added weightit.fit as a usable function with help page

--- R/weightit.fit.R
+++ R/weightit.fit.R
@@ -1,11 +1,85 @@
-weightit.fit <- function(covs, treat, method, treat.type, s.weights = rep(1, length(treat)), by.factor = factor(rep(1, length(treat))),
+weightit.fit <- function(covs, treat, method = "ps", s.weights = NULL, by.factor = NULL,
                          estimand = "ATE", focal = NULL, stabilize = FALSE, ps = NULL, moments = NULL, int = FALSE,
-                         subclass = NULL, is.MSM.method = FALSE, missing = if (anyNA(covs)) "ind" else "", include.obj = FALSE, ...){
+                         subclass = NULL, is.MSM.method = FALSE, missing = NULL, include.obj = FALSE, ...){
 
   #main function of weightit that dispatches to weightit2method and returns object containing weights and ps
-  out <- make_list(c("weights", "ps", "fit.obj"))
 
-  treat.type <- match_arg(treat.type, c("binary", "multinomial", "continuous"))
+  #Checks
+  if (!check_if_call_from_fun(weightit) && !check_if_call_from_fun(weightitMSM)) {
+
+    if (missing(covs) || !is.matrix(covs) || !is.numeric(covs)) {
+      stop("'covs' must be a numeric matrix of covariates.", call. = FALSE)
+    }
+
+    if (missing(treat) || is_not_null(dim(treat)) || !is.atomic(treat)) {
+      stop("'treat' must be an atomic vector.", call. = FALSE)
+    }
+    else if (length(treat) != nrow(covs)) {
+      stop("'treat' and 'covs' must contain the same number of units.", call. = FALSE)
+    }
+    if (anyNA(treat)) {
+      stop("No missing values are allowed in 'treat'.", call. = FALSE)
+    }
+    if (!has.treat.type(treat)) treat <- assign.treat.type(treat)
+    treat.type <- get.treat.type(treat)
+
+    check.acceptable.method(method, msm = FALSE, force = FALSE)
+
+    if (is_not_null(ps)) {
+      if (!is.numeric(ps) || is_not_null(dim(ps))) {
+        stop("'ps' must be a numeric vector.", call. = FALSE)
+      }
+      else if (length(ps) != length(treat)) {
+        stop("'ps' and 'treat' must be the same length.", call. = FALSE)
+      }
+      method <- "ps"
+    }
+
+    if (is_null(s.weights)) s.weights <- rep(1, length(treat))
+    else if (!is.numeric(s.weights) || is_not_null(dim(s.weights))) {
+      stop("'s.weights' must be a numeric vector.", call. = FALSE)
+    }
+    else if (length(s.weights) != length(treat)) {
+      stop("'s.weights' and 'treat' must be the same length.", call. = FALSE)
+    }
+
+    if (is_null(by.factor)) by.factor <- factor(rep(1, length(treat)), levels = 1)
+    else if (!is.factor(by.factor)) {
+      stop("'by.factor' must be a factor.", call. = FALSE)
+    }
+    else if (length(by.factor) != length(treat)) {
+      stop("'by.factor' and 'treat' must be the same length.", call. = FALSE)
+    }
+
+    #Process estimand and focal
+    estimand <- process.estimand(estimand, method, treat.type)
+    f.e.r <- process.focal.and.estimand(focal, estimand, treat, treat.type)
+    focal <- f.e.r[["focal"]]
+    estimand <- f.e.r[["estimand"]]
+
+    if (is_null(missing)) {
+      if (anyNA(covs)) missing <- "ind"
+      else missing <- ""
+    }
+    else if (!is.character(missing) || is_not_null(dim(missing))) {
+      stop("'missing' must be NULL or a character vector.", call. = FALSE)
+    }
+    else if (missing != "" && anyNA(covs)) {
+      missing <- process.missing(missing, method, treat.type)
+    }
+    else missing <- ""
+
+    #Check subclass
+    if (is_not_null(subclass)) check.subclass(method, treat.type)
+
+    #Process moments and int
+    moments.int <- process.moments.int(moments, int, method)
+    moments <- moments.int[["moments"]]; int <- moments.int[["int"]]
+  }
+
+  out <- make_list(c("weights", "ps", "fit.obj", "info"))
+  out$weights <- out$ps <- rep(NA_real_, length(treat))
+
   if (include.obj) fit.obj <- make_list(levels(by.factor))
   info <- make_list(levels(by.factor))
 
@@ -327,12 +401,15 @@ weightit.fit <- function(covs, treat, method, treat.type, s.weights = rep(1, len
     #   }
     #   else stop("Kernel balancing is not compatible with continuous treatments.", call. = FALSE)
     # }
+    else {
+      stop("Invalid argument to 'method'.", call. = FALSE)
+    }
 
     #Extract weights
     if (is_null(obj)) stop("No object was created. This is probably a bug,\n     and you should report it at https://github.com/ngreifer/WeightIt/issues.", call. = FALSE)
     if (is_null(obj$w) || all(is.na(obj$w))) warning("No weights were estimated. This is probably a bug,\n     and you should report it at https://github.com/ngreifer/WeightIt/issues.", call. = FALSE)
     if (any(!is.finite(obj$w))) {
-      warning("Some weights were estimated as NA, which means a value was impossible to compute (e.g., Inf). Check for extreme values of the treatment or covariates and try removing them. NA weights will be set to 0.", call. = FALSE)
+      warning("Some weights were estimated as NA, which means a value was impossible to compute (e.g., Inf). Check for extreme values of the treatment or covariates and try removing them. Non-finite weights will be set to 0.", call. = FALSE)
       obj$w[!is.finite(obj$w)] <- 0
     }
     # else if (any(!is.finite(obj$w))) probably.a.bug()
@@ -353,5 +430,8 @@ weightit.fit <- function(covs, treat, method, treat.type, s.weights = rep(1, len
   if (is_not_null(info) && nlevels(by.factor) == 1) info <- info[[1]]
   out$info <- info
 
+  if (all(is.na(out$ps))) out$ps <- NULL
+
+  class(out) <- "weightit.fit"
   return(out)
 }
