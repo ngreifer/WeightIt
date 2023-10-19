d542870bfa44354846bc5cb9abcef2d18034708d
Noah Greifer <noah.greifer@gmail.com>
2023-10-19 11:22:26 -0400

    Added package help file

---
+++ R/WeightIt-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import stats
+#' @import ggplot2
+#' @importFrom utils data combn hasName
+#' @importFrom graphics boxplot plot par hist
+#' @importFrom crayon italic underline strikethrough
+## usethis namespace: end
+NULL
