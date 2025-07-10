bb350a1c95dc6c5dfd6e5675d153668253fe47a1
Noah Greifer <noah.greifer@gmail.com>
2025-07-10 16:02:53 -0400

    Added treat class and methods to be able to retain attributes

---
+++ R/treat.R
@@ -0,0 +1,34 @@
+#Treatment class
+#' @exportS3Method `[` treat
+`[.treat` <- function(x, ..., value) {
+  y <- NextMethod("[")
+  attr(y, "treat.type") <- attr(x, "treat.type")
+  attr(y, "treat.name") <- attr(x, "treat.name")
+  attr(y, "treated") <- attr(x, "treated")
+  attr(y, "control") <- attr(x, "control")
+
+  class(y) <- class(x)
+
+  y
+}
+
+as.treat <- function(x, process = NULL) {
+  if (is_null(process)) {
+    process <- !inherits(x, "treat")
+  }
+
+  chk::chk_flag(process)
+
+  if (process || !has_treat_type(x)) {
+    x <- assign_treat_type(x)
+    treat.type <- get_treat_type(x)
+
+    if (treat.type == "multi-category") {
+      x <- assign_treat_type(factor(x))
+    }
+  }
+
+  class(x) <- unique(c("treat", class(x)))
+
+  x
+}
